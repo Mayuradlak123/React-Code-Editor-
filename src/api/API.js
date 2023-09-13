23e9497066fdd99f4d0144e9cbd79b556e37ee90
Mayur Adlak <99585635+Mayuradlak123@users.noreply.github.com>
2023-09-13 10:31:14 +0530

    Initial Commit

---
+++ src/api/API.js
@@ -0,0 +1,3 @@
+export const REACT_APP_RAPID_API_URL="https://judge0-ce.p.rapidapi.com/submissions";
+export const REACT_APP_RAPID_API_HOST="judge0-ce.p.rapidapi.com";
+export const REACT_APP_RAPID_API_KEY="a05dc01bc4msh4ce44a752e32965p1ff6dfjsn1ddc20b5e1bd";
